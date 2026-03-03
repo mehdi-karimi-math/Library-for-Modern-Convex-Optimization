adb7d2db80ca3165b2cf9f56d317d1d5f86ae67b
mehdi-karimi-math <mahdikarimi1982@gmail.com>
2026-03-03 13:07:06 -0600

    Add files via upload

--- Hyperbolic Programming/Create_Tables_Paper.m
+++ Hyperbolic Programming/Create_Tables_Paper.m
@@ -6,7 +6,7 @@
 %% Levent Tuncel
 %% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-
+short_version = true;
 
 scriptDir_table = fileparts(mfilename('fullpath'))
 
@@ -14,23 +14,26 @@ scriptDir_table = fileparts(mfilename('fullpath'))
 % Creating Table 1 in the paper. The result will be saved in the current 
 % folder as a file 'Table-1.txt', given in Latex format. 
 
-%% run(fullfile(scriptDir_table,'Entropy-ESP\HB_entropy_Table'));
+% run(fullfile(scriptDir_table,'Entropy-ESP\HB_entropy_Table'));
 
 % Creating Table 2 in the paper. The result will be saved in the current 
 % folder as a file 'Table-2.txt', given in Latex format. 
 
-%% run(fullfile(scriptDir_table,'Projection-Hyper Cone\Proj_DDS_FW_Table'));
+scriptDir = fileparts(mfilename('fullpath'));
+addpath(fullfile(scriptDir, 'solver'));  % This if for adding the Frank-Wolfe code if it was already downloaded and added to the folder. 
+
+run(fullfile(scriptDir_table,'Projection-Hyper Cone\Proj_DDS_FW_Table'));
 
 % Creating Table 3 in the paper. The result will be saved in the current 
 % folder as a file 'Table-3.txt', given in Latex format. 
 
-%% run(fullfile(scriptDir_table,'Vamos-like Hyper Poly\Proj_DDS_Vamos_Table'));
+run(fullfile(scriptDir_table,'Vamos-like Hyper Poly\Proj_DDS_Vamos_Table'));
 
 
 % Creating Table 4 in the paper. The result will be saved in the current 
 % folder as a file 'Table-4.txt', given in Latex format. 
 
-%% run(fullfile(scriptDir_table,'Composite Hyperbolic\Proj_DDS_Comp_Table'));
+run(fullfile(scriptDir_table,'Composite Hyperbolic\Proj_DDS_Comp_Table'));
 
 % Creating Table 5 in the paper. The result will be saved in the current 
 % folder as a file 'Table-5.txt', given in Latex format. 
